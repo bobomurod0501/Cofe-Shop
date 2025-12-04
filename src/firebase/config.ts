7a2823a26274ce10bfe1daabd0fd45fcf7f8a4e1
Bobomurod <bobomurodeshmahmatov@gmail.com>
2025-12-04 17:12:44 +0500

    changed

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAkobVLEZ4MeL6J2a_-f4QDiB5Ie_bQ99M",
-  authDomain: "kofe-shop.firebaseapp.com",
-  projectId: "kofe-shop",
-  storageBucket: "kofe-shop.firebasestorage.app",
-  messagingSenderId: "1003489565292",
-  appId: "1:1003489565292:web:855dabb74eb56ee6f11649",
-  measurementId: "G-HC909GM78Y",
+  apiKey: "AIzaSyBnjBRz-9pGjkYgWE9lr--D29ITnkfrAVk",
+  authDomain: "online-cofe-shop.firebaseapp.com",
+  projectId: "online-cofe-shop",
+  storageBucket: "online-cofe-shop.firebasestorage.app",
+  messagingSenderId: "570082587776",
+  appId: "1:570082587776:web:77ecd39ad609581107a265",
+  measurementId: "G-NFVPKR6F8J",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
- export const db = getFirestore(app);
- export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
